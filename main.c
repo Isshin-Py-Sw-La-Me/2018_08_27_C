3ea6d9b291313435649e3dd4d34bf0aa439be217
isshin_toyonaga <isshin@isshin-hope.local>
2018-08-27 18:56:18 +0900

    Edited

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
     printf("hello,World\n");
+    int a,b;
+    char c;
+    a = 2;
+    b = 3;
+    c = 'A';
+    printf("a=%d\n",a);
+    printf("c=%c\n",c);
     return 0;
 }
