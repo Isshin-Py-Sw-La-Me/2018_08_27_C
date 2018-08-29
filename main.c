c1e6f5e8a57523f8cfd331ecbcdaa13568d5cd4a
ToyoShin <wet.tissue.love.110@gmail.com>
2018-08-29 10:07:20 +0900

    Edited

--- main.c
+++ main.c
@@ -10,6 +10,15 @@ int main(){
     printf("c=%c\n",c);
     float d = 0.5;
     printf("d=%f\n",d);
+    
+    double radius = 10; //半径
+    double pi = 3.14;
+    double circumference;
+    double area;
 
+    circumference = radius * 2 * pi;
+    area = radius * radius * pi;
+    // double を表示するには%f
+    printf("Area is %f\n",area);
     return 0;
 }
